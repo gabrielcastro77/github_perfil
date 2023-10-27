be2f8ce7da82a1d9ec804fe941cc9ce8e616e9bd
Gabriel Castro <gabrielsilvadecastro777@gmail.com>
2023-10-27 11:04:49 -0300

    commit inicial

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './global.css'
+
+// import { PI as numeroPi, GTM_BRASIL } from './teste'
+// import nomes from './nomes'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
